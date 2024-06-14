adabe128f64e40b6f5308d7e8756081746b8726e
Jaroslav Kotrba <jaroslav.kotrba@gmail.com>
2024-06-14 18:00:09 +0200

    readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# ChallengeNZZ
+# Model Creation and Analysis
+
+This repository contains the necessary steps to set up the environment, install required packages, and run the scripts for creating and analyzing models using a provided dataset.
+
+## Installation
+
+### Step 1: Copy the Dataset
+Copy the `dataset_propensity.json` file to the `data` folder.
+
+### Step 2: Create and Activate Conda Environment
+Create a new Conda environment with Python 3.10:
+```sh
+conda create -n nzz python=3.10
+conda activate nzz
+```
+
+### Step 3: Install libraries
+Run in activated env
+```sh
+pip install pandas
+pip install matplotlib
+pip install seaborn
+pip install plotly
+pip install nbformat
+pip install scikit_learn
+pip install xgboost
+pip install shap
+```
+
+### Step 4: Create clean dataset
+Run ../analysis/analysis.py to create clean_data.csv
+```sh
+python analysis.py
+```
+
+### Step 5: Create model
+Run ../src/main.py to create model_pipe.csv
+```sh
+python main.py
+```
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@
 # copy dataset_propensity.json to 'data' folder
 # conda create -n nzz python=3.10
 # conda env remove --name nzz
+
 # pip install pandas
 # pip isntall matplotlib
 # pip isntall seaborn
@@ -12,6 +13,13 @@
 # pip isntall xgboost
 # pip install shap
 
+# run ../analysis/analysis.py
+# run ../src/main.py
+
+# REQUIREMENTS
+# pip freeze > requirements.txt
+
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.compose import make_column_transformer
@@ -31,6 +39,7 @@ from sklearn.metrics import (
 )
 from xgboost import XGBClassifier
 import shap
+import joblib
 
 pd.set_option("display.max_rows", 500)
 pd.set_option("display.max_columns", 100)
@@ -166,12 +175,95 @@ shap.summary_plot(
     shap_values, X_test_transformed, feature_names=all_features, max_display=30
 )
 
+### SAVE MODEL
+
+joblib.dump(pipe, "../model/model_pipe.pkl")
+
+### PREDICTION
+loaded_pipe = joblib.load("../model/model_pipe.pkl")
+
+
+def sample(pipe, X_test):
+    X_sample = np.array(
+        [
+            "de",
+            "International",
+            "Windows",
+            "evening",
+            8,
+            3,
+            0,
+            0.0,
+            21.0,
+            3.0,
+            17.0,
+            1.0,
+            1,
+            483,
+            4,
+            0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            1.0,
+            0.0,
+            3.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            1.0,
+            0.0,
+            0,
+            0.0,
+            1,
+            0,
+            1,
+            5,
+            5,
+            20,
+            9,
+            20,
+            4,
+            21,
+            0,
+            0,
+            190150,
+            0,
+            0,
+            0,
+            2,
+            4,
+            0,
+            0,
+            0,
+            0,
+            0,
+            3,
+            4,
+            0,
+        ]
+    )
+
+    X_sample = pd.DataFrame(X_sample.reshape(1, -1))
+    X_sample.columns = X_test.columns
+
+    y_pred = pipe.predict_proba(X_sample)[:, 1]  # probability for the positive class
+
+    print(f"Sample prediction - probability for the purchase: {y_pred[0]:.4f}")
+
+
+sample(pipe, X_test)
+
 # Features on the Y-axis: The list of features on the Y-axis shows the most important features at the top. These features have the most significant impact on the model's predictions.
 # SHAP values on the X-axis: The X-axis represents the SHAP values, which show the impact of each feature on the model's output. A positive SHAP value indicates that the feature contributes positively towards predicting the target variable (e.g., a higher probability of a user buying), while a negative SHAP value indicates a negative contribution.
 # The colors represent the feature values. Red represents higher values of the feature, and blue represents lower values. This helps to understand how the value of a feature influences its impact on the prediction.
 # Each dot in the plot represents a single prediction from the test dataset. The spread of dots shows the distribution of SHAP values for that feature across all predictions.
 
-# Understanding the Factors:
+# -----------------------------------------------------------------------------------------------------------
+# Understanding the Factors: --------------------------------------------------------------------------------
+
 # nzz: This feature has a high impact on the model's predictions, with both high and low values (red and blue dots) influencing the SHAP values significantly (long shape).
 # num_read_articles: Higher values (red dots) of this feature tend to move the SHAP values to the left, indicating that users who read more articles are less likely to buy. Conversely, lower values (blue dots) have a neutral or slightly positive impact on the prediction, contributing to a higher likelihood of buying.
 # number_of_newsletters: This feature also has a significant impact, with higher values increasing the probability of buying.
@@ -182,4 +274,49 @@ shap.summary_plot(
 # mostLikedCategories_NotAvailable: This categorical feature shows that if the preferred category is not available, it negatively affects the likelihood of buying.
 # num_display_articles: The number of display articles also has a variable impact, with higher values contributing both positively and negatively in different contexts.
 
-# Model Improvements:
+# -----------------------------------------------------------------------------------------------------------
+# Model Improvements: ---------------------------------------------------------------------------------------
+
+# Feature Engineering
+# Interaction Features: Create new features by combining existing features. For example, interactions between num_read_articles and time_from_last_session.
+# Extract time-based features such as day of the week, month, or season if applicable.
+
+# Hyperparameter Tuning
+# Perform extensive hyperparameter tuning using techniques like Grid Search or Random Search with cross-validation.
+# Use other machine learning algorithms like Random Forest, Gradient Boosting Machine (GBM), or CatBoost
+
+# Feature Selection
+# Remove non-significant variables
+# Principal Component Analysis Non-Linear Dimensionality Reduction
+
+# -----------------------------------------------------------------------------------------------------------
+# Deployment Steps: -----------------------------------------------------------------------------------------
+
+# Final Model Training
+
+# Select Deployment Environment (Heroku, Shiny Server, GCP)
+
+# Create Virtual Environment
+
+# Containerization
+
+# Create a Web Service Flask/Shiny
+
+# Deployment
+
+# Configure CI/CD Pipeline GitHub Actions
+
+# -----------------------------------------------------------------------------------------------------------
+# Collaboration Strategies: ---------------------------------------------------------------------------------
+
+# Schedule initial meetings to understand the business goals, key performance indicators (KPIs), and specific problems the machine learning solution should address.
+
+# Hold regular meetings (e.g., weekly or bi-weekly) to update the business team on progress, challenges, and next steps.
+
+# Use visual aids such as dashboards, charts, and presentations to make technical concepts accessible.
+
+# Use Agile methodologies, such as Scrum or Kanban, to ensure iterative development and continuous feedback.
+
+# Create early prototypes or minimum viable products (MVPs) to demonstrate the potential of the solution.
+
+# Conduct UAT sessions where the business team can interact with the solution, provide feedback, and identify any issues before full deployment.
